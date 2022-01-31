0786030a809d32bbae7ea69f641cfa3266ecbfe0
Sagar <sagarpaulkar@gmail.com>
2022-01-31 12:13:35 +0530

    first commit

---
+++ src/test/java/qaclickacademy/ApiTest.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiTest {
+
+
+@Test
+
+public void postJira()
+
+{
+	System.out.println("postJira");
+}
+
+
+@Test
+public void deleteTwitter()
+{
+	
+	System.out.println("deleteTwitter");
+}
+
+
+}
---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+
+	public void NativeappAndroid()
+
+	{
+		System.out.println("NativeappAndroid");
+	}
+
+
+	@Test
+	public void IOSapp()
+	{
+		
+		System.out.println("IOSapp");
+	}
+}
